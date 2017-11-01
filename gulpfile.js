76764a17638cadc20123a420feee6815177d835a
li-monica <386468183@qq.com>
2017-11-01 15:56:50 +0800

    修改ip地址

--- gulpfile.js
+++ gulpfile.js
@@ -9,7 +9,7 @@ gulp.task("webserver",function(){
         .pipe(webserver({
             livereload:true,    // 浏览器自动刷新，更新数据，类似热替换
             port:8080,          // 自定义端口号
-            host:"192.168.2.105",    // 主机,可以更换成电脑IP
+            host:"192.168.2.104",    // 主机,可以更换成电脑IP
             directoryListing:{   // 要不要在浏览器中显示你开发环境得项目目录,便于开发使用，如果上线，就必须设置为false
                 enable:true ,     //true 显示 默认false
                 path:"./"         //作用的文件目录范围
