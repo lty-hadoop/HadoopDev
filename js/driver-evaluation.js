e916b6096aa91c371baf57b09126d851d34ea49f
incarhu <jacksonhyl@163.com>
2017-11-07 14:35:00 +0800

    司机评价页面

---
+++ js/driver-evaluation.js
@@ -0,0 +1,47 @@
+/*
+* 司机评价
+* */
+$(function(){
+    getEchartsData ('js/chartData.json', 5, '{value}');
+
+    var myChart = echarts.init(document.getElementById('driver'));
+    var option = {
+        title: {
+            text: '基础雷达图'
+        },
+        tooltip: {},
+        legend: {
+            data: ['预算分配（Allocated Budget）']
+        },
+        radar: {
+            // shape: 'circle',
+            name: {
+                textStyle: {
+                    color: '#fff',
+                    backgroundColor: '#999',
+                    borderRadius: 3,
+                    padding: [3, 5]
+                }
+            },
+            indicator: [
+                { name: '销售（sales）', max: 6500},
+                { name: '管理（Administration）', max: 16000},
+                { name: '信息技术（Information Techology）', max: 30000},
+                { name: '客服（Customer Support）', max: 38000},
+                { name: '研发（Development）', max: 52000},
+                { name: '市场（Marketing）', max: 25000}
+            ]
+        },
+        series: [
+            {
+                type: 'radar',
+                data: [
+                    {name: '预算', value: [1,2,3,4,5]}
+                ]
+            }
+        ]
+    };
+
+    myChart.setOption(option);
+
+});
