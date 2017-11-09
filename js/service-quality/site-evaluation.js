9a09ad2b25ed45a3b8084d02d379e9fc5095927a
incarhu <jacksonhyl@163.com>
2017-11-09 18:22:52 +0800

    时间控件联动

--- js/service-quality/site-evaluation.js
+++ js/service-quality/site-evaluation.js
@@ -6,12 +6,31 @@ $(function(){
 
     statis('js/public/evaluation.json');
 
-
-    $('#test').jeDate({
+    var start = {
         format: 'YYYY-MM-DD hh:mm:ss',
-        toggle: function(obj){
-            console.log(obj.elem);     //得到当前输入框的ID
-            console.log(obj.val);      //得到日期生成的值，如：2017-06-16
+        maxDate: $.nowDate({DD:0}), //最大日期
+        okfun: function(obj){
+            end.minDate = obj.val; //开始日选好后，重置结束日的最小日期
+            endDates();
+            console.log(end.minDate)
         }
-    })
+    };
+    var end = {
+        format: 'YYYY-MM-DD hh:mm:ss',
+        minDate: $.nowDate({DD:0}), //设定最小日期为当前日期
+        okfun: function(obj){
+            start.maxDate = obj.val; //将结束日的初始值设定为开始日的最大日期
+            console.log(start.maxDate)
+        }
+    };
+//这里是日期联动的关键
+    function endDates() {
+        //将结束日期的事件改成 false 即可
+        end.trigger = false;
+        $("#test1").jeDate(end);
+    }
+    $('#test').jeDate(start);
+    $('#test1').jeDate(end);
+
+    console.log(end.minDate)
 });
