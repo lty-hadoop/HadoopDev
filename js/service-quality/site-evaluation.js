bef3df3e55f25dcdaf31afc64572593c933df2d0
incarhu <jacksonhyl@163.com>
2017-11-09 16:35:20 +0800

    时间控件

--- js/service-quality/site-evaluation.js
+++ js/service-quality/site-evaluation.js
@@ -5,4 +5,13 @@ $(function(){
     getEchartsData ('js/chartData.json', 5, '{value}')
 
     statis('js/public/evaluation.json');
+
+
+    $('#test').jeDate({
+        format: 'YYYY-MM-DD hh:mm:ss',
+        toggle: function(obj){
+            console.log(obj.elem);     //得到当前输入框的ID
+            console.log(obj.val);      //得到日期生成的值，如：2017-06-16
+        }
+    })
 });
