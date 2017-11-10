ad4b8ab9b5a589078021a2d93d34ec75fb5cd164
incarhu <jacksonhyl@163.com>
2017-11-10 15:32:29 +0800

    新增表格控件

--- js/service-quality/site-evaluation.js
+++ js/service-quality/site-evaluation.js
@@ -32,5 +32,39 @@ $(function(){
     $('#test').jeDate(start);
     $('#test1').jeDate(end);
 
-    console.log(end.minDate)
+    console.log(end.minDate);
+
+    //表格控件
+    $("#testtable").jeTable({
+        width:"100%",
+        height:300,
+        datas:{
+            url:"js/tableData.json",
+            type:"GET",
+            async:false,
+            dataType:"json",
+            field: 'sites'
+        },
+        isPage: false,
+        pageField:{
+            ellipsis:true,
+            dataCount:"totalCount",
+            pageCount:"totalPage"
+        },
+        columnSort:[1,2,3,4,5],
+        columns:[
+            {name:'评价时间',field: 'time', width:"80",isShow:true},
+            { name:'评价地点',field: 'address',width:"550"},
+            {name:'用户名', field: 'user', width:"150"},
+            { name:'评价等级',field: 'state',width:"200"},
+            { name:'具体内容', field: 'content',width:"200"}
+        ],
+        success:function (elem) {
+            // elem.find("tr").on("click",function () {
+            //     var trdata = $.parseJSON($(this).attr("trdata"));
+            //     console.log(trdata)
+            // })
+            console.log(elem)
+        }
+    })
 });
